fd48681a752090469a11882c753059188d3ef725
a-korkin <a-korkin88@yandex.ru>
2024-10-26 14:04:42 +0300

    feat: tutors done

---
+++ tutor-web-app-ssr/src/iter5/dbaccess.rs
@@ -0,0 +1,40 @@
+use crate::iter5::errors::EzyTutorError;
+use crate::iter5::model::*;
+use sqlx::postgres::PgPool;
+
+pub async fn get_user_record(
+    pool: &PgPool, 
+    username: String,
+) -> Result<User, EzyTutorError> {
+    let user_row = sqlx::query_as!(
+        User,
+        "select * from ezyweb_user where username = $1",
+        username
+    )
+    .fetch_optional(pool)
+    .await?;
+
+    if let Some(user) = user_row {
+        Ok(user)
+    } else {
+        Err(EzyTutorError::NotFound("Username not found".into()))
+    }
+}
+
+pub async fn post_new_user(
+    pool: &PgPool,
+    new_user: User,
+) -> Result<User, EzyTutorError> {
+    let user_row = sqlx::query_as!(
+        User,
+        "insert into ezyweb_user(username, tutor_id, user_password)
+        values($1, $2, $3)
+        returning username, tutor_id, user_password",
+        new_user.username, new_user.tutor_id, new_user.user_password
+    )
+    .fetch_one(pool)
+    .await?;
+
+    Ok(user_row)
+}
+
--- tutor-web-app-ssr/src/iter5/handler.rs
+++ tutor-web-app-ssr/src/iter5/handler.rs
@@ -1,11 +1,74 @@
-use actix_web::{Error, HttpResponse, Result};
+use actix_web::{web, Error, HttpResponse, Result};
+use argon2::Config;
+use crate::iter5::errors::EzyTutorError;
+use crate::iter5::dbaccess::{get_user_record, post_new_user};
+use serde_json::json;
 
-pub async fn show_register_form() -> Result<HttpResponse, Error> {
-    let msg = "Hello, you are in the registration page";
-    Ok(HttpResponse::Ok().content_type("text/html").body(msg))
+use super::model::TutorRegisterForm;
+use super::state::AppState;
+
+pub async fn show_register_form(
+    tmpl: web::Data<tera::Tera>
+) -> Result<HttpResponse, Error> {
+    let mut ctx = tera::Context::new();
+    ctx.insert("error", "");
+    ctx.insert("current_username", "");
+    ctx.insert("current_password", "");
+    ctx.insert("current_confirmation", "");
+    ctx.insert("current_name", "");
+    ctx.insert("current_imageurl", "");
+    ctx.insert("current_profile", "");
+
+    let s = tmpl
+        .render("register.html", &ctx)
+        .map_err(|_| EzyTutorError::TeraError("Template error".to_string()))?;
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
-pub async fn handle_register() -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().body(""))
+pub async fn handle_register(
+    tmpl: web::Data<tera::Tera>,
+    app_state: web::Data<AppState>,
+    params: web::Form<TutorRegisterForm>,
+) ->Result<HttpResponse, Error> {
+    let mut ctx = tera::Context::new();
+    let mut s: String;
+    let username = params.username.clone();
+    let user = get_user_record(&app_state.db, username.to_string()).await;
+    let user_not_found: bool = user.is_err();
+
+    if user_not_found {
+        if params.password != params.confirmation {
+            ctx.insert("error", "Passowrds do not match");
+            s = tmpl
+                .render("register.html", &ctx)
+                .map_err(|_| EzyTutorError::TeraError("Template error".to_string()))?;
+        } else {
+            let new_tutor = json!({
+            });
+            let awc_client = awc::Client::default();
+            let res = awc_client
+                .post("http://localhost:3000/tutors/")
+                .send_json(&new_tutor)
+                .await
+                .unwrap()
+                .body()
+                .await?;
+            let tutor_response = serde_json::from_str(&std::str::from_utf8(&res)?)?;
+            s = format!("Congratulations.");
+            let salt = b"somerandomsalt";
+            let config = Config::default();
+            let hash = argon2::hash_encoded(
+                params.password.clone().as_bytes(), salt, &config).unwrap();
+            let user = User {};
+            let _tutor_created = post_new_user(&app_state.db, user).await;
+        }
+    } else {
+        ctx.insert("error", "User Id already exists");
+        s = tmpl
+            .render("register.html", &ctx)
+            .map_err(|_| EzyTutorError::TeraError("Template error".to_string()))?;
+    };
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
